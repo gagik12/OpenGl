338943557429114d392e6e8a303a7078e2aad18c
Vadim <romanof.romanoff@yandex.ru>
2016-09-18 15:18:58 +0300

    Update function.cpp

--- function/function.cpp
+++ function/function.cpp
@@ -2,16 +2,29 @@
 #include <glm\glm.hpp>
 #include <stdlib.h>
 #include <math.h>
-static const int WINDOW_WIDTH = 1200;
+
+static const int WINDOW_WIDTH = 700;
 static const int WINDOW_HEIGHT = 672;
-		
+
+#define ESCAPE '\033'
+
 void DrawCoordinateSystem()
 {
+	glColor3d(0, 0, 0);
 	glBegin(GL_LINES);
 	glVertex2d(-10, 0);
 	glVertex2d(10, 0);
 	glVertex2d(0, -1.5);
 	glVertex2d(0, 1.5);
+	glEnd();
+}
+
+void DrawFunction()
+{
+	glColor3d(0, 0, 1);
+	glBegin(GL_LINE_STRIP);
+	for (double i = -10; i < 10; i += 0.1)
+		glVertex2d(i, sin(i) / i);
 	glEnd();
 }
 
@@ -22,24 +35,16 @@ void Display()
 	glLoadIdentity();
 	glClear(GL_COLOR_BUFFER_BIT);
 	glScalef(0.06, 0.6, 1);
-	glColor3d(0, 0, 0);
 
 	DrawCoordinateSystem();
+	DrawFunction();
 
-	glColor3d(0, 0, 1);
-	glBegin(GL_LINE_STRIP);
-	for (double i = -10; i < 10; i += 0.1)
-		glVertex2d(i, sin(i)/i);
-	glEnd();
-	glFinish();
+	glFlush();
 }
 
-void Reshape(GLint w, GLint h)
+void Reshape(GLint width, GLint height)
 {
-	int width = w;
-	int weight = h;
-
-	glViewport(0, 0, w, h);
+	glViewport(0, 0, width, height);
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -51,9 +56,10 @@ void Reshape(GLint w, GLint h)
 
 void Keyboard(unsigned char key, int x, int y)
 {
-#define ESCAPE '\033'
 	if (key == ESCAPE)
+	{
 		exit(0);
+	}
 }
 
 void main(int argc, char* argv[])
