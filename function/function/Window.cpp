5fe9fa210440e84c7231a8c26243b9e34c671914
Vadim <romanof.romanoff@yandex.ru>
2016-09-18 21:55:59 +0300

    Update Window.cpp

--- function/function/Window.cpp
+++ function/function/Window.cpp
@@ -12,14 +12,21 @@
 
 namespace
 {
+	static const glm::vec2 SCALE(0.05f, 0.3f);
+	static const glm::vec2 BORDER(-10.f, 10.f);
 
-	void DrawFunction(const glm::ivec2 & size)
+	glm::vec2 GetCenterWindow(const glm::ivec2 & size)
+	{
+		return glm::vec2(size.x / 2, size.y / 2);
+	}
+
+	void DrawFunction(const glm::ivec2 & center)
 	{
 		glBegin(GL_LINE_STRIP);
-		for (float x = -10; x <= 10; x += 0.1)
+		for (float x = BORDER.x; x <= BORDER.y; x += 0.1)
 		{
 			float y = std::sin(x) / x;
-			glVertex2d(size.x/2 + x*20.f, size.y / 2 - y *70.f);
+			glVertex2d(center.x + x*SCALE.x *center.x / 2, center.y - y *SCALE.y * center.y);
 		}
 		glEnd();
 	}
@@ -27,7 +34,6 @@ namespace
 	void DrawCoordinateSystem(const glm::ivec2 & size)
 	{
 		glBegin(GL_LINES);
-
 		    glVertex2f(0.0f, size.y / 2.f);
 	        glVertex2f(size.x, size.y / 2.f);
 
@@ -63,7 +69,8 @@ void CWindow::OnDrawWindow(const glm::ivec2 & size)
 	glColor3f(1, 0, 0);
 	DrawCoordinateSystem(size);
 	glColor3f(1, 0, 0);
-	DrawFunction(size);
+	glm::vec2 windowCenter = GetCenterWindow(size);
+	DrawFunction(windowCenter);
 }
 
 void CWindow::SetupView(const glm::ivec2 & size)
